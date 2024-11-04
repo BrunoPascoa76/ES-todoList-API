ab9ab6b0fc90ad5eef54f186292489f1d5590b29
BrunoPascoa76 <brunopascoa03@ua.pt>
2024-11-04 09:51:32 +0000

    KAN-2 did basic homepage ui for listing

--- app/apis/ui.py
+++ app/apis/ui.py
@@ -1,6 +1,8 @@
 from flask import make_response, redirect, render_template, request, url_for
 from flask_restx import Namespace, Resource
 
+from services.auth_service import get_user
+
 
 api=Namespace("ui",description="UI-related endpoints")
 
@@ -8,6 +10,9 @@ api=Namespace("ui",description="UI-related endpoints")
 class HomePage(Resource):
     def get(self):
         if "access_token" in request.cookies:
-            return make_response(render_template("homePage.html"),200,{"Content-type":"text/html"})
+            token=request.cookies.get("access_token")
+            user=get_user(token)
+            tasks=get_tasks_by_user_id(user["Username"])
+            return make_response(render_template("homePage.html",tasks=tasks),200,{"Content-type":"text/html"})
         else:
             return redirect(url_for("auth_login"))
